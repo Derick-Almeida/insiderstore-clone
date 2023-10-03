3e248e7e644bd3a699c06d25559dee1f5a34588c
Derick-Almeida <d.a.s2000silva14@gmail.com>
2023-10-03 19:50:44 -0300

    feat: database

---
+++ src/database/product.ts
@@ -0,0 +1,175 @@
+import blue_color_clothes_1 from "../assets/blue_color_clothes_1.jpg";
+import blue_color_clothes_2 from "../assets/blue_color_clothes_2.jpg";
+import blue_color_clothes_3 from "../assets/blue_color_clothes_3.jpg";
+import blue_color_clothes_4 from "../assets/blue_color_clothes_4.jpg";
+import blue_color_clothes_5 from "../assets/blue_color_clothes_5.jpg";
+import blue_color_clothes_6 from "../assets/blue_color_clothes_6.jpg";
+import black_color_clothes_1 from "../assets/black_color_clothes_1.jpg";
+import black_color_clothes_2 from "../assets/black_color_clothes_2.jpg";
+import black_color_clothes_3 from "../assets/black_color_clothes_3.jpg";
+import black_color_clothes_4 from "../assets/black_color_clothes_4.jpg";
+import black_color_clothes_5 from "../assets/black_color_clothes_5.jpg";
+import indigo_color_clothes_1 from "../assets/indigo_color_clothes_1.jpg";
+import indigo_color_clothes_2 from "../assets/indigo_color_clothes_2.jpg";
+import indigo_color_clothes_3 from "../assets/indigo_color_clothes_3.jpg";
+import indigo_color_clothes_4 from "../assets/indigo_color_clothes_4.jpg";
+import indigo_color_clothes_5 from "../assets/indigo_color_clothes_5.jpg";
+import indigo_color_clothes_6 from "../assets/indigo_color_clothes_6.jpg";
+import blue_shadow_color_clothes_1 from "../assets/blue-shadow_color_clothes_1.jpg";
+import blue_shadow_color_clothes_2 from "../assets/blue-shadow_color_clothes_2.jpg";
+import blue_shadow_color_clothes_3 from "../assets/blue-shadow_color_clothes_3.jpg";
+import blue_shadow_color_clothes_4 from "../assets/blue-shadow_color_clothes_4.jpg";
+import nori_green_color_clothes_1 from "../assets/nori-green_color_clothes_1.png";
+import nori_green_color_clothes_2 from "../assets/nori-green_color_clothes_2.png";
+import nori_green_color_clothes_3 from "../assets/nori-green_color_clothes_3.png";
+import nori_green_color_clothes_4 from "../assets/nori-green_color_clothes_4.png";
+import nori_green_color_clothes_5 from "../assets/nori-green_color_clothes_5.png";
+import nori_green_color_clothes_6 from "../assets/nori-green_color_clothes_6.png";
+import carmenere_color_clothes_1 from "../assets/carmenere_color_clothes_1.jpg";
+import carmenere_color_clothes_2 from "../assets/carmenere_color_clothes_2.jpg";
+import carmenere_color_clothes_3 from "../assets/carmenere_color_clothes_3.jpg";
+import carmenere_color_clothes_4 from "../assets/carmenere_color_clothes_4.jpg";
+import carmenere_color_clothes_5 from "../assets/carmenere_color_clothes_5.jpg";
+import carmenere_color_clothes_6 from "../assets/carmenere_color_clothes_6.jpg";
+import mescla_color_clothes_1 from "../assets/mescla_color_clothes_1.jpg";
+import mescla_color_clothes_2 from "../assets/mescla_color_clothes_2.jpg";
+import mescla_color_clothes_3 from "../assets/mescla_color_clothes_3.jpg";
+import mescla_color_clothes_4 from "../assets/mescla_color_clothes_4.jpg";
+import mescla_color_clothes_5 from "../assets/mescla_color_clothes_5.jpg";
+import off_white_color_clothes_1 from "../assets/off-white_color_clothes_1.jpg";
+import off_white_color_clothes_2 from "../assets/off-white_color_clothes_2.jpg";
+import off_white_color_clothes_3 from "../assets/off-white_color_clothes_3.jpg";
+import off_white_color_clothes_4 from "../assets/off-white_color_clothes_4.jpg";
+import off_white_color_clothes_5 from "../assets/off-white_color_clothes_5.jpg";
+import off_white_color_clothes_6 from "../assets/off-white_color_clothes_6.jpg";
+import wash_yellow_color_clothes_1 from "../assets/wash-yellow_color_clothes_1.jpg";
+import wash_yellow_color_clothes_2 from "../assets/wash-yellow_color_clothes_2.jpg";
+import wash_yellow_color_clothes_3 from "../assets/wash-yellow_color_clothes_3.jpg";
+import wash_yellow_color_clothes_4 from "../assets/wash-yellow_color_clothes_4.jpg";
+import graywood_color_clothes_1 from "../assets/graywood_color_clothes_1.jpg";
+import graywood_color_clothes_2 from "../assets/graywood_color_clothes_2.jpg";
+import graywood_color_clothes_3 from "../assets/graywood_color_clothes_3.jpg";
+
+const data = {
+  colors: [
+    {
+      id: "4261643c-47e2-43fc-b5c4-2290c7090882",
+      color: "blue",
+      hex: "#33405b",
+      image: [
+        blue_color_clothes_1,
+        blue_color_clothes_2,
+        blue_color_clothes_3,
+        blue_color_clothes_4,
+        blue_color_clothes_5,
+        blue_color_clothes_6,
+      ],
+    },
+    {
+      id: "49386320-96e6-4039-a867-f754c1d0320e",
+      color: "black",
+      hex: "#000000",
+      image: [
+        black_color_clothes_1,
+        black_color_clothes_2,
+        black_color_clothes_3,
+        black_color_clothes_4,
+        black_color_clothes_5,
+      ],
+    },
+    {
+      id: "6e52b6ed-c4e2-4694-8842-86de6bf54bb9",
+      color: "indigo",
+      hex: "#576091",
+      image: [
+        indigo_color_clothes_1,
+        indigo_color_clothes_2,
+        indigo_color_clothes_3,
+        indigo_color_clothes_4,
+        indigo_color_clothes_5,
+        indigo_color_clothes_6,
+      ],
+    },
+    {
+      id: "72870e49-da0f-4ddf-9750-74c786759f05",
+      color: "blue shadow",
+      hex: "#95b7c4",
+      image: [
+        blue_shadow_color_clothes_1,
+        blue_shadow_color_clothes_2,
+        blue_shadow_color_clothes_3,
+        blue_shadow_color_clothes_4,
+      ],
+    },
+    {
+      id: "1b29b88c-3972-4158-8020-b95d1f55bf77",
+      color: "nori green",
+      hex: "#525041",
+      image: [
+        nori_green_color_clothes_1,
+        nori_green_color_clothes_2,
+        nori_green_color_clothes_3,
+        nori_green_color_clothes_4,
+        nori_green_color_clothes_5,
+        nori_green_color_clothes_6,
+      ],
+    },
+    {
+      id: "666af63a-8b54-44b0-b341-e91e6d130f66",
+      color: "carmenere",
+      hex: "#572531",
+      image: [
+        carmenere_color_clothes_1,
+        carmenere_color_clothes_2,
+        carmenere_color_clothes_3,
+        carmenere_color_clothes_4,
+        carmenere_color_clothes_5,
+        carmenere_color_clothes_6,
+      ],
+    },
+    {
+      id: "6158ab0a-3357-4148-9f0d-33dc96121197",
+      color: "mescla",
+      hex: "#696969",
+      image: [
+        mescla_color_clothes_1,
+        mescla_color_clothes_2,
+        mescla_color_clothes_3,
+        mescla_color_clothes_4,
+        mescla_color_clothes_5,
+      ],
+    },
+    {
+      id: "4c7c17e7-25c6-4837-a436-d1734df8fcdd",
+      color: "off white",
+      hex: "#f0f0ed",
+      image: [
+        off_white_color_clothes_1,
+        off_white_color_clothes_2,
+        off_white_color_clothes_3,
+        off_white_color_clothes_4,
+        off_white_color_clothes_5,
+        off_white_color_clothes_6,
+      ],
+    },
+    {
+      id: "07edd080-5bb2-443d-9368-042f31032948",
+      color: "wash yellow",
+      hex: "#e0b865",
+      image: [
+        wash_yellow_color_clothes_1,
+        wash_yellow_color_clothes_2,
+        wash_yellow_color_clothes_3,
+        wash_yellow_color_clothes_4,
+      ],
+    },
+    {
+      id: "02a4499c-95d3-4bd8-9ab9-52387a5e2fcc",
+      color: "graywood",
+      hex: "#8d8070",
+      image: [graywood_color_clothes_1, graywood_color_clothes_2, graywood_color_clothes_3],
+    },
+  ],
+};
+
+export default data;
