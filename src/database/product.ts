3935e8f6ae37eb8840bfa4ac43700e0e5fb581ac
Derick-Almeida <d.a.s2000silva14@gmail.com>
2023-10-04 09:05:52 -0300

    fix: updated database

--- src/database/product.ts
+++ src/database/product.ts
@@ -51,11 +51,37 @@ import graywood_color_clothes_2 from "../assets/graywood_color_clothes_2.jpg";
 import graywood_color_clothes_3 from "../assets/graywood_color_clothes_3.jpg";
 
 const data = {
-  colors: [
+  name: "Tech T-Shirt Gola V",
+  reviews: 1415,
+  stars: 4.8,
+  clothes: [
     {
       id: "4261643c-47e2-43fc-b5c4-2290c7090882",
       color: "blue",
       hex: "#33405b",
+      price: 165,
+      sizes: [
+        {
+          id: "c6a445be-5784-4a6f-901c-fd6fc333ca93",
+          size: "P",
+          inStock: true,
+        },
+        {
+          id: "af3f7859-d0f2-4b9c-913a-7c25316432cd",
+          size: "M",
+          inStock: true,
+        },
+        {
+          id: "7e4e2ddf-5766-4974-93e6-6d64ab69aad6",
+          size: "GG",
+          inStock: true,
+        },
+        {
+          id: "5e40d4c9-8d51-4d9a-a011-ec0d8844e2e8",
+          size: "XGG",
+          inStock: false,
+        },
+      ],
       image: [
         blue_color_clothes_1,
         blue_color_clothes_2,
@@ -69,6 +95,44 @@ const data = {
       id: "49386320-96e6-4039-a867-f754c1d0320e",
       color: "black",
       hex: "#000000",
+      price: 165,
+      sizes: [
+        {
+          id: "58823034-bcb6-4a9a-880b-3e89aa9c2fb6",
+          size: "P",
+          inStock: true,
+        },
+        {
+          id: "ffba51fd-78b7-4fed-a12b-9f7bcb1ef62d",
+          size: "M",
+          inStock: true,
+        },
+        {
+          id: "0d6c642c-373c-443c-8639-57236ba00be6",
+          size: "G",
+          inStock: true,
+        },
+        {
+          id: "87e55e65-9d56-4059-876c-eb9e762f2ff2",
+          size: "GG",
+          inStock: true,
+        },
+        {
+          id: "fb6a7df4-ffa5-4699-a0a8-709c7ef614fa",
+          size: "XGG",
+          inStock: true,
+        },
+        {
+          id: "df9e1812-0b1d-4540-b679-f1ee0bced28a",
+          size: "PP",
+          inStock: true,
+        },
+        {
+          id: "4632cfcf-2600-4664-b4d8-6a400a5fef35",
+          size: "XXGG",
+          inStock: false,
+        },
+      ],
       image: [
         black_color_clothes_1,
         black_color_clothes_2,
@@ -81,6 +145,34 @@ const data = {
       id: "6e52b6ed-c4e2-4694-8842-86de6bf54bb9",
       color: "indigo",
       hex: "#576091",
+      price: 169,
+      sizes: [
+        {
+          id: "11ce9fd6-451d-4d3b-b9b0-7bc64068b1c7",
+          size: "P",
+          inStock: true,
+        },
+        {
+          id: "f9732a5f-6950-4e9b-8759-1028ae603ac2",
+          size: "M",
+          inStock: true,
+        },
+        {
+          id: "bd029add-e78d-4f47-b617-e60dc04f68d9",
+          size: "G",
+          inStock: true,
+        },
+        {
+          id: "068ee848-7bb0-4ec5-88bb-be054d84771a",
+          size: "GG",
+          inStock: true,
+        },
+        {
+          id: "5a2b2f95-4560-4c0f-a5ed-4a2f105fb5de",
+          size: "XGG",
+          inStock: false,
+        },
+      ],
       image: [
         indigo_color_clothes_1,
         indigo_color_clothes_2,
@@ -94,6 +186,69 @@ const data = {
       id: "72870e49-da0f-4ddf-9750-74c786759f05",
       color: "blue shadow",
       hex: "#95b7c4",
+      price: 165,
+      sizes: [
+        {
+          id: "72c1830c-f736-461e-b9e4-9ffebb41433e",
+          size: "P",
+          inStock: true,
+        },
+        {
+          id: "ccd216c4-12a0-441e-a2e2-792a14254367",
+          size: "M",
+          inStock: true,
+        },
+        {
+          id: "1f2e886f-70df-421c-8a57-2e7f7f726784",
+          size: "G",
+          inStock: true,
+        },
+        {
+          id: "c437391c-8afd-444b-a049-78ca438835e5",
+          size: "GG",
+          inStock: true,
+        },
+        {
+          id: "4f897c2a-ea7a-47cb-8594-63414fea686d",
+          size: "XGG",
+          inStock: true,
+        },
+        {
+          id: "ab8a73cf-655f-4a32-8fff-3013188503ee",
+          size: "PP",
+          inStock: true,
+        },
+        {
+          id: "08e8090b-b7c7-4ab2-a858-7ead8fe3804c",
+          size: "XXGG",
+          inStock: true,
+        },
+        {
+          id: "8de65778-fbc1-4007-9c49-374bdbf8e23b",
+          size: "P",
+          inStock: true,
+        },
+        {
+          id: "507f292c-f510-4c6f-92b9-87c188ec0940",
+          size: "M",
+          inStock: true,
+        },
+        {
+          id: "09dd3090-c597-4b9e-a2eb-78084cbae927",
+          size: "G",
+          inStock: true,
+        },
+        {
+          id: "7536ddec-639a-4390-8476-b46adbe1a134",
+          size: "GG",
+          inStock: true,
+        },
+        {
+          id: "73c6ddfa-1880-463e-8515-baf48aae39b9",
+          size: "XGG",
+          inStock: true,
+        },
+      ],
       image: [
         blue_shadow_color_clothes_1,
         blue_shadow_color_clothes_2,
@@ -105,6 +260,34 @@ const data = {
       id: "1b29b88c-3972-4158-8020-b95d1f55bf77",
       color: "nori green",
       hex: "#525041",
+      price: 169,
+      sizes: [
+        {
+          id: "fa55c474-3df8-4dd3-93d0-3bcd89317709",
+          size: "P",
+          inStock: true,
+        },
+        {
+          id: "7258fd85-2d99-4a01-a38b-19883a14c39b",
+          size: "M",
+          inStock: true,
+        },
+        {
+          id: "70233c0d-b620-4c31-a44f-6eac0b9d26b7",
+          size: "G",
+          inStock: true,
+        },
+        {
+          id: "6344da08-1a86-4b58-ab67-7dc5729645d3",
+          size: "GG",
+          inStock: true,
+        },
+        {
+          id: "7174f333-60dd-4139-8bcf-bdc36e50e222",
+          size: "XGG",
+          inStock: true,
+        },
+      ],
       image: [
         nori_green_color_clothes_1,
         nori_green_color_clothes_2,
@@ -118,6 +301,34 @@ const data = {
       id: "666af63a-8b54-44b0-b341-e91e6d130f66",
       color: "carmenere",
       hex: "#572531",
+      price: 169,
+      sizes: [
+        {
+          id: "1f6d3155-6da7-4a1c-93b8-fe37e9dae193",
+          size: "P",
+          inStock: true,
+        },
+        {
+          id: "31db0df5-fff0-4e70-8b37-1dce498ee7a6",
+          size: "M",
+          inStock: true,
+        },
+        {
+          id: "1d8e23fe-bf06-443f-8f01-1bf3660670fb",
+          size: "G",
+          inStock: true,
+        },
+        {
+          id: "6f596fce-ee1d-4836-aa55-b1fbb274bcb0",
+          size: "GG",
+          inStock: false,
+        },
+        {
+          id: "d8f09e33-e28c-4af0-bc44-c2e19a81ed92",
+          size: "XGG",
+          inStock: true,
+        },
+      ],
       image: [
         carmenere_color_clothes_1,
         carmenere_color_clothes_2,
@@ -131,6 +342,34 @@ const data = {
       id: "6158ab0a-3357-4148-9f0d-33dc96121197",
       color: "mescla",
       hex: "#696969",
+      price: 165,
+      sizes: [
+        {
+          id: "c3b10415-7cef-4a08-a240-4184c87d0c92",
+          size: "P",
+          inStock: true,
+        },
+        {
+          id: "31758b5a-1e53-4d4a-a10d-a825062c641b",
+          size: "M",
+          inStock: true,
+        },
+        {
+          id: "b456b14e-00b0-46b8-9523-e1fd988748c6",
+          size: "G",
+          inStock: true,
+        },
+        {
+          id: "53da9544-f11f-4129-9393-2c6c405bf80a",
+          size: "GG",
+          inStock: false,
+        },
+        {
+          id: "38b0f58e-982f-4ca8-9be5-07715a6a44cd",
+          size: "XGG",
+          inStock: true,
+        },
+      ],
       image: [
         mescla_color_clothes_1,
         mescla_color_clothes_2,
@@ -143,6 +382,39 @@ const data = {
       id: "4c7c17e7-25c6-4837-a436-d1734df8fcdd",
       color: "off white",
       hex: "#f0f0ed",
+      price: 165,
+      sizes: [
+        {
+          id: "f9bfee14-0638-485a-9baa-3d4c6c95019b",
+          size: "P",
+          inStock: true,
+        },
+        {
+          id: "eb8d8e05-69b3-4466-9e63-d3443003a083",
+          size: "M",
+          inStock: false,
+        },
+        {
+          id: "d3987911-5643-42cc-9790-8e21e7e08769",
+          size: "G",
+          inStock: false,
+        },
+        {
+          id: "3e58fa68-671f-4d0f-ba0a-8e542f9f5933",
+          size: "GG",
+          inStock: false,
+        },
+        {
+          id: "7d942267-fcf8-48c1-97d2-28ee3532be84",
+          size: "XGG",
+          inStock: true,
+        },
+        {
+          id: "b78bba86-a9c3-433c-8d1a-9ab3a4be6d5f",
+          size: "PP",
+          inStock: true,
+        },
+      ],
       image: [
         off_white_color_clothes_1,
         off_white_color_clothes_2,
@@ -156,6 +428,19 @@ const data = {
       id: "07edd080-5bb2-443d-9368-042f31032948",
       color: "wash yellow",
       hex: "#e0b865",
+      price: 169,
+      sizes: [
+        {
+          id: "877d3486-4d44-42a0-8b7b-0777b09cdd73",
+          size: "G",
+          inStock: true,
+        },
+        {
+          id: "c24625a7-483a-4e14-a163-32527e120969",
+          size: "PP",
+          inStock: true,
+        },
+      ],
       image: [
         wash_yellow_color_clothes_1,
         wash_yellow_color_clothes_2,
@@ -167,6 +452,14 @@ const data = {
       id: "02a4499c-95d3-4bd8-9ab9-52387a5e2fcc",
       color: "graywood",
       hex: "#8d8070",
+      price: 169,
+      sizes: [
+        {
+          id: "302869f2-dfa3-46b9-b6a5-cd7983a2f1db",
+          size: "PP",
+          inStock: true,
+        },
+      ],
       image: [graywood_color_clothes_1, graywood_color_clothes_2, graywood_color_clothes_3],
     },
   ],
