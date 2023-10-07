a2615191c8ad1fcc6f561441498d3ec1151b07e3
Derick-Almeida <d.a.s2000silva14@gmail.com>
2023-10-07 18:49:59 -0300

    image slider logic

--- src/components/Gallery/style.ts
+++ src/components/Gallery/style.ts
@@ -16,3 +16,44 @@ export const main = styled.main`
     height: fit-content;
   }
 `;
+
+export const span = styled.span`
+  background-color: var(--white);
+  border-radius: 50%;
+  padding: 8px;
+
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+
+  position: absolute;
+  top: 15px;
+  left: 15px;
+
+  opacity: 0;
+  font-size: 14px;
+
+  > svg {
+    opacity: 0.7;
+  }
+`;
+
+export const box = styled.a`
+  min-width: 100%;
+  min-height: auto;
+  background: var(--gray-2) no-repeat center;
+  background-size: cover;
+  position: relative;
+
+  > img {
+    width: 100%;
+  }
+
+  &:hover {
+    cursor: pointer;
+
+    > ${span} {
+      opacity: 1;
+    }
+  }
+`;
