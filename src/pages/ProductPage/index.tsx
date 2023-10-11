5a21f31038a7f200238c2cd82145ecfd0180051b
Derick-Almeida <d.a.s2000silva14@gmail.com>
2023-10-10 21:31:05 -0300

    feat: mobile header

--- src/pages/ProductPage/index.tsx
+++ src/pages/ProductPage/index.tsx
@@ -1,15 +1,30 @@
 import * as S from "./style";
+import { useEffect, useState } from "react";
 
 import Header from "../../components/Header";
 import Gallery from "../../components/Gallery";
 import Footer from "../../components/Footer";
 import ProductInfo from "../../components/ProductInfo";
 import MoreProductInfo from "../../components/MoreProductInfo";
+import MobileHeader from "../../components/MobileHeader";
 
 const ProductPage = () => {
+  const [windowSize, setWindowSize] = useState<number>(0);
+
+  useEffect(() => {
+    setWindowSize(window.outerWidth);
+
+    const noteTheSize = () => {
+      setWindowSize(window.outerWidth);
+    };
+    window.addEventListener("resize", noteTheSize);
+
+    return () => {};
+  }, []);
+
   return (
     <>
-      <Header />
+      {windowSize > 425 ? <Header /> : <MobileHeader />}
 
       <S.section>
         <Gallery />
